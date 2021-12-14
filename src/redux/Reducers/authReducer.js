b1c94c7ed479db4a3cb5caf27a02d8f1cddfe733
parvezaman <emailtoamanullah@gmail.com>
2021-12-14 13:01:37 +0600

    store added

---
+++ src/redux/Reducers/authReducer.js
@@ -0,0 +1,16 @@
+
+
+const initialState ={
+    isLoggedIn: false,
+    user: null,
+    user_id: null
+};
+
+const authReducer = (state=initialState, {type, payload})=>{
+    switch(type){
+        default:
+            return state;
+    }
+};
+
+export default authReducer;
