404515d1463ab2a2fc85e1e20ad8a9c8f70a5c24
parvezaman <emailtoamanullah@gmail.com>
2021-12-17 21:50:03 +0600

    log out method added

--- src/redux/ActionCreators/AuthAuctionCreator.js
+++ src/redux/ActionCreators/AuthAuctionCreator.js
@@ -6,8 +6,14 @@ const setUser = (data)=>({
     type: types.SET_USER,
     payload:data
 });
+const resetUser = ()=>({
+    type: types.RESET_USER
+});
 
 // Create auth action
 export const loginUser = data => dispatch =>{
     dispatch(setUser(data));
+};
+export const logOutUser = () => dispatch =>{
+    dispatch(resetUser());
 };
